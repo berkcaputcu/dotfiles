e14148202e29a5acfd9c8299b4727a2e5a4713fc
berkcaputcu <berk.caputcu@shopify.com>
2016-04-09 16:46:10 -0400

    tidy up

--- home/.zshrc
+++ home/.zshrc
@@ -1,20 +1,18 @@
-export ZSH=/home/vagrant/.oh-my-zsh
+export ZSH=~/.oh-my-zsh
 
 ZSH_THEME="robbyrussell"
 
 HIST_STAMPS="yyyy-mm-dd"
 
-ZSH_CUSTOM=~/src/scripts
+ZSH_CUSTOM=~/dotfiles/zsh-custom
 
-plugins=(git bgnotify)
+plugins=(git bgnotify zsh-256color)
 
 source $ZSH/oh-my-zsh.sh
 
-  export PATH="/home/vagrant/.gem/ruby/2.1.6/bin:/usr/lib/shopify-ruby/2.1.6-shopify2/lib/ruby/gems/2.1.0/bin:/usr/lib/shopify-ruby/2.1.6-shopify2/bin:/home/vagrant/.linuxbrew/bin:/home/vagrant/bin:/home/vagrant/.nvm/versions/node/v5.5.0/bin:/usr/local/heroku/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/vagrant/src/go/bin"
-
-
-
-export NVM_DIR="/home/vagrant/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-
 export PRY=1
+
+[ -f /opt/dev/dev.sh ] && source /opt/dev/dev.sh
+
+export NVM_DIR="/Users/berkcaputcu/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
---
+++ zsh-custom/profile.zsh
@@ -0,0 +1,74 @@
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+alias to="ruby ~/dotfiles/scripts/testonly.rb"
+alias be="bundle exec"
+alias rtest="ruby -Itest"
+
+alias cds="cd ~/code/Shopify"
+alias cdc="cd ~/code/"
+
+alias gl='git log --graph --pretty=format:"%Creset%h %aN %Cred%d %Cblue%s"'
+alias gd='git diff HEAD --color'
+alias ggg="git grep -n -5"
+alias gg="git grep -n"
+alias gb="git branch"
+alias gbranches="git for-each-ref refs/heads --format='%(align:70)%(refname:short)%(end) %(committerdate:iso8601) (%(committerdate:relative))' --sort -committerdate | head -n30"
+alias git-clean="git remote prune origin;git branch --merged master | grep -v \"master\" | xargs -n 1 git branch -d"
+
+alias vssh='cd ~/code/vagrant/ && vagrant ssh'
+alias vmount='cd ~/code/vagrant/src/'
+
+bundle-up () {
+  git checkout master
+  git pull origin master
+
+  bin/bootstrap
+  bundle exec rake db:migrate
+}
+
+ggfp () {
+  git push origin +$(current_branch)
+}
+
+amend-last () {
+  local answer
+
+  if [[ $(current_branch) = 'master' ]]; then
+    echo 'Switch to a branch to amend'
+    return
+  fi
+
+  git diff HEAD
+  git add .
+
+  vared -p 'Would you like to force push?: ' -c answer
+  if [[ $answer = 'y' ]]; then
+    git commit --amend
+    git push origin +$(current_branch)
+  fi
+}
+
+rebase-master() {
+  local answer
+  local prev_branch=$(current_branch)
+
+  if [[ $prev_branch = 'master' ]]; then
+    echo 'Switch to a branch to rebase'
+    return
+  fi
+
+  git checkout master
+  git pull origin master
+  git checkout $prev_branch
+  git rebase master
+
+  git push origin $prev_branch
+
+  vared -p 'Would you like to force push?: ' -c answer
+  if [[ $answer = 'y' ]]; then
+    echo $prev_branch
+    git push origin +$prev_branch
+  fi
+}
